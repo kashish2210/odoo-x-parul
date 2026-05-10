cc512e96ee95a2df3eb66279285ccd945648dd40
kashish shrivastav <en.shrivastavkashish@gmail.com>
2026-05-10 13:34:41 +0530

    admin panel

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
+    path('admin-panel/', views.admin_panel_view, name='admin_panel'),
     path('', views.dashboard_view, name='dashboard'),
 ]
